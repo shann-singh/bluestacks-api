89f47c6a2cb3cec54ed22953352982c6a38b3f81
shann-singh <shann.singh@gmail.com>
2021-06-04 23:31:18 +0530

    second commit

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const config = require("./utilities/config");
 const { log } = require("./utilities/logger");
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Welcome to bluestacks-api");
+});
+
 app.listen(config.port, () => {
   console.log(`Express server is listening on ${config.port}`);
 });
